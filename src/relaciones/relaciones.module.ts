48f98c2c1bb6d25a26a437765950b8a527077c68
JavierQuinan <javierquiandra@gmail.com>
2024-03-22 19:15:51 -0500

    cambios

--- src/relaciones/relaciones.module.ts
+++ src/relaciones/relaciones.module.ts
@@ -1,40 +1,13 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { VIAJE, DESTINO, GUIA, TURISTA } from 'src/models/models';
-import { DestinoSchema, GuiaSchema, TuristaSchema, ViajeSchema } from './schema/relacion.schema';
-import { RelacionController } from './relaciones.controller';
-import { RelacionService } from './relaciones.service';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forFeatureAsync([
-      {
-        name: VIAJE.name,
-        useFactory: () => {
-          return ViajeSchema;
-        },
-      },
-      {
-        name: DESTINO.name,
-        useFactory: () => {
-          return DestinoSchema;
-        },
-      },
-      {
-        name: GUIA.name,
-        useFactory: () => {
-          return GuiaSchema;
-        },
-      },
-      {
-        name: TURISTA.name,
-        useFactory: () => {
-          return TuristaSchema;
-        },
-      },
+    MongooseModule.forFeature([
+      // Importa aquí los esquemas necesarios
     ]),
   ],
-  controllers: [RelacionController],
-  providers: [RelacionService],
+  controllers: [RelacionesController],
+  providers: [RelacionesService],
 })
 export class RelacionModule {}
